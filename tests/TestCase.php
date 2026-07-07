e8ddcaae85fb5a1f5287fa4b9048714cb93bc6fb
charlietyn <charlietyn@gmail.com>
2026-07-08 00:36:00 +0100

    Format base test case for Pint

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,4 +1,14 @@
 <?php
+
 namespace Ronu\LaravelFederatedAuth\Tests;
-use Orchestra\Testbench\TestCase as Orchestra; use Ronu\LaravelFederatedAuth\FederatedAuthServiceProvider;
-class TestCase extends Orchestra { protected function getPackageProviders($app): array { return [FederatedAuthServiceProvider::class]; } }
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use Ronu\LaravelFederatedAuth\FederatedAuthServiceProvider;
+
+class TestCase extends Orchestra
+{
+    protected function getPackageProviders($app): array
+    {
+        return [FederatedAuthServiceProvider::class];
+    }
+}
