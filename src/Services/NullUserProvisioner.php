0d9717d3eba542c7313cfa2a7f28fd31f0c1199f
charlietyn <charlietyn@gmail.com>
2026-07-08 00:52:42 +0100

    Apply Pint style to null user provisioner

--- src/Services/NullUserProvisioner.php
+++ src/Services/NullUserProvisioner.php
@@ -1,4 +1,17 @@
 <?php
+
 namespace Ronu\LaravelFederatedAuth\Services;
-use Illuminate\Contracts\Auth\Authenticatable; use Ronu\LaravelFederatedAuth\Contracts\UserProvisionerInterface; use Ronu\LaravelFederatedAuth\DTO\AuthContext; use Ronu\LaravelFederatedAuth\DTO\ExternalIdentity; use Ronu\LaravelFederatedAuth\Exceptions\UserProvisioningNotConfiguredException;
-class NullUserProvisioner implements UserProvisionerInterface { public function provision(ExternalIdentity $identity, AuthContext $context): Authenticatable { throw new UserProvisioningNotConfiguredException('Auto provisioning is enabled but no UserProvisionerInterface implementation was configured.'); } }
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Ronu\LaravelFederatedAuth\Contracts\UserProvisionerInterface;
+use Ronu\LaravelFederatedAuth\DTO\AuthContext;
+use Ronu\LaravelFederatedAuth\DTO\ExternalIdentity;
+use Ronu\LaravelFederatedAuth\Exceptions\UserProvisioningNotConfiguredException;
+
+class NullUserProvisioner implements UserProvisionerInterface
+{
+    public function provision(ExternalIdentity $identity, AuthContext $context): Authenticatable
+    {
+        throw new UserProvisioningNotConfiguredException('Auto provisioning is enabled but no UserProvisionerInterface implementation was configured.');
+    }
+}
