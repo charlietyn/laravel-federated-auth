ab73a8fb463f80164589edb6ac3c3aad1bab9255
charlietyn <charlietyn@gmail.com>
2026-07-08 00:53:45 +0100

    Apply Pint style to Socialite provider wrappers

--- src/Providers/GoogleProviderAdapter.php
+++ src/Providers/GoogleProviderAdapter.php
@@ -1,3 +1,11 @@
 <?php
+
 namespace Ronu\LaravelFederatedAuth\Providers;
-class GoogleProviderAdapter extends SocialiteProviderAdapter { public function name(): string { return 'google'; } }
+
+class GoogleProviderAdapter extends SocialiteProviderAdapter
+{
+    public function name(): string
+    {
+        return 'google';
+    }
+}
