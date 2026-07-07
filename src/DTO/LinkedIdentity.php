ebcafe6e1e50957c8ee9a5b3fb856d27157492c7
charlietyn <charlietyn@gmail.com>
2026-07-08 00:45:22 +0100

    Apply Pint style to LinkedIdentity DTO

--- src/DTO/LinkedIdentity.php
+++ src/DTO/LinkedIdentity.php
@@ -1,3 +1,15 @@
 <?php
+
 namespace Ronu\LaravelFederatedAuth\DTO;
-final class LinkedIdentity { public function __construct(public readonly string|int $id, public readonly string|int $userId, public readonly string $provider, public readonly string $providerUserId, public readonly ?string $tenantId=null, public readonly array $attributes=[]) {} }
+
+final class LinkedIdentity
+{
+    public function __construct(
+        public readonly string|int $id,
+        public readonly string|int $userId,
+        public readonly string $provider,
+        public readonly string $providerUserId,
+        public readonly ?string $tenantId = null,
+        public readonly array $attributes = [],
+    ) {}
+}
